f654c865ff709592a2bcd70fc43e0ea690632c51
helisoya <oudot.julien@laposte.net>
2025-02-20 16:57:04 +0100

    Fix DontDestroyOnLoad

--- Assets/_Project/Sounds/MusicPlayer.cs
+++ Assets/_Project/Sounds/MusicPlayer.cs
@@ -8,18 +8,18 @@ public class MusicPlayer : MonoBehaviour
     public static MusicPlayer instance; // Singleton instance
 
     private EventInstance myMusicInstance;
-    
+
     private void Awake()
     {
         // Ensure this is the only instance
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Persist across scenes
+            //DontDestroyOnLoad(gameObject); // Persist across scenes
         }
         else
         {
-            Destroy(gameObject); // Destroy duplicate
+            //Destroy(gameObject); // Destroy duplicate
         }
     }
 
@@ -38,7 +38,7 @@ public class MusicPlayer : MonoBehaviour
 
     public void OutofMenu()
     {
-       // myMusicInstance.setParameterByName("menu", 0);
+        // myMusicInstance.setParameterByName("menu", 0);
     }
 
     public void BackToMenu()
@@ -50,15 +50,16 @@ public class MusicPlayer : MonoBehaviour
         //FMODUnity.RuntimeManager.StudioSystem.setParameterByName("filter EQ", 0f);
     }
 
-    public void Replay(){
-        
+    public void Replay()
+    {
+
         //myMusicInstance.setParameterByName("menu", 0);  
         //myMusicInstance.setParameterByName("victory", 0);
-       // myMusicInstance.setParameterByName("defeat", 0);
-       // myExplosionInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-       // _dashCooldownEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        // myMusicInstance.setParameterByName("defeat", 0);
+        // myExplosionInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        // _dashCooldownEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         //_dashCanalisationEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-       // AudioManager.instance.ambienceEventInstance.setParameterByName("filter EQ", 0);
+        // AudioManager.instance.ambienceEventInstance.setParameterByName("filter EQ", 0);
         //FMODUnity.RuntimeManager.StudioSystem.setParameterByName("filter EQ", 0f);
     }
 
@@ -71,9 +72,9 @@ public class MusicPlayer : MonoBehaviour
 
     public void DefeatMusic()
     {
-       // myMusicInstance.setParameterByName("defeat", 1);
-       // AudioManager.instance.ambienceEventInstance.setParameterByName("filter EQ", 1);
-       // FMODUnity.RuntimeManager.StudioSystem.setParameterByName("filter EQ", 1.0f);
+        // myMusicInstance.setParameterByName("defeat", 1);
+        // AudioManager.instance.ambienceEventInstance.setParameterByName("filter EQ", 1);
+        // FMODUnity.RuntimeManager.StudioSystem.setParameterByName("filter EQ", 1.0f);
     }
 
 }
