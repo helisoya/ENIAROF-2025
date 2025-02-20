e733c66ec7e473627df936f658df7f257a9f322f
helisoya <oudot.julien@laposte.net>
2025-02-20 15:07:25 +0100

    fixes

--- Assets/_Project/Scripts/Quiz/Content/CoverElement.cs
+++ Assets/_Project/Scripts/Quiz/Content/CoverElement.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CoverElement", menuName = "ENIAROF/CoverElement")]
 public class CoverElement : ScriptableObject
 {
+
 	public enum CoverElementType
 	{
 		BACKGROUNDCOLOR,
